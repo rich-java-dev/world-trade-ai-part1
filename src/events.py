f260f5a6339d847049afb5015bf29a9f0b48ec99
Rich White <rwhite@edmundsgovtech.com>
2022-02-20 16:22:32 -0500

    progress on trade impl

--- src/events.py
+++ src/events.py
@@ -35,7 +35,7 @@ class Action(ABC):
         return False
 
     @abstractmethod
-    def apply(self, state: WorldState, **kwargs) -> int:
+    def apply(self, state: WorldState, **kwargs):
         return 0
 
 
@@ -47,10 +47,6 @@ class Transform(Action):
     # "Flux" like pattern, taking in a current state
     def apply(self, state: WorldState, **kwargs) -> int:
         return 0
-
-
-class Transfer(Action):
-    pass
 
 
 '''
@@ -168,8 +164,91 @@ class HousingTemplate(Transform):
         return factor
 
 
+'''
+Basic Transfer Template/Skeleton Implementation:
+
+Potential Enhancements: Offer multi-Resource Trade Transactions:
+
+
+
+{
+    c1: 0, -- Country 1 in Trade Proposition
+    c2: 1, -- Country 2 in Trade Proposition
+
+    c1_offer: { -- Country 1 Trade Offer
+        resource: 'R3'
+        quantity: 100, 
+    },
+
+    c2_offer: { -- Country 2 Trade Offer (Proposed By Country 1 Currently)
+        resource:'R21',
+        quantity: 10,
+    }
+}
+
+'''
+
+
+class Transfer(Action):
+
+    # Ensure Both Parties of required resources for said transfer
+    def is_viable(self, world: WorldState, **kwargs) -> bool:
+
+        c1_idx: int = kwargs['c1']
+        c2_idx: int = kwargs['c2']
+
+        c1: Country = world.countries[c1_idx]
+        c1_offer: dict = kwargs['c1_offer']
+        c1_offer_rsrc: str = c1.offer.resource
+        c1_offer_qty: int = c1_offer.quantity
+
+        c2: Country = world.countries[c2_idx]
+        c2_offer: dict = kwargs['c2_offer']
+        c2_offer_rsrc: str = c2.offer.resource
+        c2_offer_qty: int = c2_offer.quantity
+
+        # abort if trade resource is infeasible
+        if c1_offer_qty <= 0 or c2_offer_qty <= 0:
+            return False
+
+        # c1 cannot attempt to trade more than c1's resource quantity
+        if c1_offer_qty > c1.resources[c1_offer_rsrc].quantity:
+            return False
+
+        # c2 cannot attempt to trade more than c1's resource quantity
+        if c2_offer_qty > c2.resources[c2_offer_rsrc].quantity:
+            return False
+
+        # return true if both Countries could 'feasibly' trade resource request
+        return True
+
+    def apply(self, world: WorldState, **kwargs) -> str:
+
+        c1_idx: int = kwargs['c1']
+        c2_idx: int = kwargs['c2']
+
+        c1: Country = world.countries[c1_idx]
+        c1_offer: dict = kwargs['c1_offer']
+        c1_offer_rsrc = c1.offer.resource
+        c1_offer_qty = c1_offer.quantity
+
+        c2: Country = world.countries[c2_idx]
+        c2_offer: dict = kwargs['c2_offer']
+        c2_offer_rsrc = c2.offer.resource
+        c2_offer_qty = c2_offer.quantity
+
+        # Subtract resources from both countries, and then redistribute
+        c1.resources[c1_offer_rsrc] -= c1_offer_qty
+        c2.resources[c2_offer_rsrc] -= c2_offer_qty
+
+        # reflect quantities:
+        c1.resources[c2_offer_rsrc] += c2_offer_qty
+        c2.resources[c1_offer_rsrc] += c1_offer_qty
+
+
 action_map: dict = {
     'Alloy Template': AlloyTemplate(),
     'Electronics Template': ElectronicsTemplate(),
     'Housing Template': HousingTemplate(),
+    'Transfer': Transfer(),
 }
